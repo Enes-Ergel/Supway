81125593c01581acf00dbaf231320d43db254d50
PascalTheza <pascal.theza@student.isfsc.be>
2024-12-06 10:26:33 +0100

    page de contact

--- contact-page.php
+++ contact-page.php
@@ -1,7 +1,43 @@
 <?php /* Template Name: Contact Page */ get_header(); ?>
 
-<div class="container">
-    <?php echo do_shortcode(the_content()); ?>
-</div>
+<?php
+if ( isset($_GET['sent']) ){
+	if ( $_GET['sent'] == '1'){
+		echo "<p> ✔ Formulario enviado correctamente</p><br>";
+	}
+	else {
+		echo "<p> Hubo un error al enviar</p><br>";
+	}
+}
+?>
+
+  <form method="post" action="<?php echo admin_url( 'admin-post.php' ) ?>" >
+
+      <div class="blockdetexte">
+	    <label  for="name">Nom et prénom</label>
+	  <input type="text" name="name" id="name" required>
+    </div>
+	
+
+    <div class="blockdetexte">
+	  <label  for="email">E-mail</label>
+	  <input type="email" name="email" id="email" required>
+    </div>
+
+	
+      <div class="blockdetexte">
+	  <label for="message">Message</label>
+	<textarea name="message" id="message" cols="30" rows="10" required></textarea>
+    </div>
+
+
+	<p><input type="checkbox" id="terms" name="terms" required> Acepto los <a href="#">Términos y Condiciones</a></p>
+
+	<br>
+	<input type="hidden" name="action" value="process_form">
+	<input type="submit" name="submit" value="Enviar">
+</form>
+
+<?php get_footer(); ?>
 
 <?php get_footer(); ?>
