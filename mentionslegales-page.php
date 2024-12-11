af62d65578feb43210c5e05c90f22ecac926bab0
PascalTheza <pascal.theza@student.isfsc.be>
2024-12-11 15:43:39 +0100

    modification

--- mentionslegales-page.php
+++ mentionslegales-page.php
@@ -5,18 +5,28 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12 col-12">
-<h1>Mentions légales</h1>
-
+<h1 classe="titrementions">Mentions légales</h1>
+<br>
+<br>
 <h3><b>Identité et coordonnées de l’éditeur responsable de ce site internet :</b></h3>
+<br>
+<br>
 <ul>
  <li>Nom de la société : Supway</li>
+ <br>
  <li>Forme juridique : Association Sans But Lucratif (ASBL)</li>
+ <br>
  <li>Adresse postale du sièce social : Rue des Étudiants 123, 1000 Bruxelles, Belgique</li>
+ <br>
  <li>Numéro de téléphone : 0495 02 83 19 </li>
+ <br>
  <li>Adresse électronique : supway@gmail.com</li>
+ <br>
  <li>Numéro BCE : BE 0213.645.879</li>
  
 </ul>
+<br>
+<br>
         </div>
    </div>
 </div>
