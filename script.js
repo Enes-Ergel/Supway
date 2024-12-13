1c9649922eaa0d8e0bc2168acb5e5d1bc78eed4d
Enes-Ergel <ergelenes24@gmail.com>
2024-12-13 12:31:13 +0100

    script eliminer

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
-jQuery(document).ready(function($) {
-    // Gestion de l'upload d'image
-    let frame;
 
-    $('#upload-photo-button').on('click', function(e) {
-        e.preventDefault();
-
-        // Si un frame existe déjà, l'ouvrir
-        if (frame) {
-            frame.open();
-            return;
-        }
-
-        // Initialiser le frame WordPress Media
-        frame = wp.media({
-            title: 'Choisir une image',
-            button: {
-                text: 'Utiliser cette image'
-            },
-            multiple: false // Ne permettre qu'une seule image
-        });
-
-        // Lorsqu'une image est sélectionnée
-        frame.on('select', function() {
-            var attachment = frame.state().get('selection').first().toJSON();
-
-            // Ajouter l'URL de l'image au champ d'entrée
-            $('#photo').val(attachment.url);
-
-            // Afficher un aperçu de l'image sélectionnée
-            $('#photo-preview').html('<img src="' + attachment.url + '" style="max-width:100%; height:auto;">');
-        });
-
-        // Ouvrir le frame
-        frame.open();
-    });
    
     jQuery(document).ready(function($) {
         $('.grid-conseillers').slick({
