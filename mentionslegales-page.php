24e732503a2419ab606a2dd2c489530e6d7c19e6
Enes-Ergel <ergelenes24@gmail.com>
2024-12-29 18:39:02 +0100

    ajout page mentions légales

---
+++ mentionslegales-page.php
@@ -0,0 +1,32 @@
+<?php /* Template Name: legal-information */ ?>
+
+<?php get_header(); ?>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="mb-5 col-lg-12 col-12">
+<h1 class="titrementions">Mentions légales</h1>
+
+<h2 class="soustitrementions"><b>Identité et coordonnées de l’éditeur responsable de ce site internet :</b></h2>
+
+<ul>
+ <li class="mentions-item" >Nom de la société : Supway</li>
+
+ <li class="mentions-item">Forme juridique : Association Sans But Lucratif (ASBL)</li>
+
+ <li class="mentions-item">Adresse postale du siège social : Rue des Étudiants 123, 1000 Bruxelles, Belgique</li>
+
+ <li class="mentions-item">Numéro de téléphone : 0495 02 83 19 </li>
+
+ <li class="mentions-item">Adresse électronique : supway@gmail.com</li>
+
+ <li class="mentions-item">Numéro BCE : BE 0213.645.879</li>
+</ul>
+
+        </div>
+   </div>
+</div>
+
+
+<?php get_footer(); ?>
